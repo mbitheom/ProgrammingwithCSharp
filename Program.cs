3c6c4a0e4ee3ee229bdd9b7bfd16fdb97dba4790
mbitheom <44084415+mbitheom@users.noreply.github.com>
2019-09-12 18:38:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AdvancedCsharpProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = Person.Person_One("Humphry");
+            
+
+        }
+       
+
+    }
+    public class Person
+    {
+        public string Name = "Johnson";
+        public void WriteName(string name)
+        {
+            Console.WriteLine("Hi " + Name + " I am " + name);
+        }
+        static public Person Person_One(string name)
+        {
+            var person = new Person();
+            person.Name = name;
+
+            return person;
+        }
+    }
+}
